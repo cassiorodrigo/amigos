a073a4407aa09dc75b566fdbe614bb0bef4845e8
Cassio Rodrigo Peluso <cassiorodrigo@gmail.com>
2022-10-28 09:27:29 +0100

    newgeneratorexample.py to showcase one generator call

---
+++ newgeneratorexample.py
@@ -0,0 +1,17 @@
+
+
+def genex():
+    for i in "Developer":
+        yield i
+
+if __name__ == "__main__":
+    i = genex()
+    print(next(i))
+    print(next(i))
+    print(next(i))
+    print(next(i))
+    print(next(i))
+    print(next(i))
+    print(next(i))
+    print(next(i))
+    print(next(i))
